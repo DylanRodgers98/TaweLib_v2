5fef5f0df17677771588a4ec982868bc2dbd971f
Dylan Rodgers <dylanirodgers@aol.com>
2019-09-14 11:58:34 +0100

    Amend ResourceFactoryImplIT to test new loan creation method

--- src/test/java/com/crowvalley/tawelib/model/resource/ResourceFactoryImplIT.java
+++ src/test/java/com/crowvalley/tawelib/model/resource/ResourceFactoryImplIT.java
@@ -1,6 +1,8 @@
 package com.crowvalley.tawelib.model.resource;
 
+import com.crowvalley.tawelib.model.Loan;
 import com.crowvalley.tawelib.service.CopyService;
+import com.crowvalley.tawelib.service.LoanService;
 import com.crowvalley.tawelib.service.ResourceService;
 import org.assertj.core.api.JUnitSoftAssertions;
 import org.junit.Test;
@@ -26,6 +28,9 @@ public class ResourceFactoryImplIT {
     private ResourceService laptopService;
 
     @Autowired
+    private LoanService loanService;
+
+    @Autowired
     private CopyService copyService;
 
     @Autowired
@@ -44,8 +49,13 @@ public class ResourceFactoryImplIT {
         Copy copy = copyService.getAll().get(0);
 
         JUnitSoftAssertions softly = new JUnitSoftAssertions();
-        softly.assertThat(copy.getResourceId()).as("Resource ID of the copy persisted to database").isEqualTo(book.getId());
-        softly.assertThat(copy.getResourceType()).as("Resource type of the copy persisted to database").isEqualTo("Book");
+        softly.assertThat(copy.getResourceId())
+                .as("Resource ID of the copy persisted to database")
+                .isEqualTo(book.getId());
+
+        softly.assertThat(copy.getResourceType())
+                .as("Resource type of the copy persisted to database")
+                .isEqualTo("Book");
     }
 
     @Test
@@ -61,8 +71,13 @@ public class ResourceFactoryImplIT {
         Copy copy = copyService.getAll().get(0);
 
         JUnitSoftAssertions softly = new JUnitSoftAssertions();
-        softly.assertThat(copy.getResourceId()).as("Resource ID of the copy persisted to database").isEqualTo(dvd.getId());
-        softly.assertThat(copy.getResourceType()).as("Resource type of the copy persisted to database").isEqualTo("Dvd");
+        softly.assertThat(copy.getResourceId())
+                .as("Resource ID of the copy persisted to database")
+                .isEqualTo(dvd.getId());
+
+        softly.assertThat(copy.getResourceType())
+                .as("Resource type of the copy persisted to database")
+                .isEqualTo("Dvd");
     }
 
     @Test
@@ -78,8 +93,13 @@ public class ResourceFactoryImplIT {
         Copy copy = copyService.getAll().get(0);
 
         JUnitSoftAssertions softly = new JUnitSoftAssertions();
-        softly.assertThat(copy.getResourceId()).as("Resource ID of the copy persisted to database").isEqualTo(laptop.getId());
-        softly.assertThat(copy.getResourceType()).as("Resource type of the copy persisted to database").isEqualTo("Laptop");
+        softly.assertThat(copy.getResourceId())
+                .as("Resource ID of the copy persisted to database")
+                .isEqualTo(laptop.getId());
+
+        softly.assertThat(copy.getResourceType())
+                .as("Resource type of the copy persisted to database")
+                .isEqualTo("Laptop");
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -87,5 +107,33 @@ public class ResourceFactoryImplIT {
     public void testCreateCopyWithNoResourceID() {
         Book book = resourceFactory.createBook("","","","","","","","");
         resourceFactory.createCopy(book, 4);
+    }
+
+    @Test
+    @Transactional
+    public void testAddCopyToDatabase_CreateLoanOfCopy() {
+        laptopService.save(resourceFactory.createLaptop("","","","","",""));
+
+        List<Laptop> laptops = laptopService.getAll();
+        Laptop laptop = laptops.get(0);
+
+        copyService.save(resourceFactory.createCopy(laptop, 4));
+
+        Copy copy = copyService.getAll().get(0);
+
+        loanService.save(resourceFactory.createLoanForCopy(copy, "DylanRodgers98"));
+        Loan loan = loanService.getAll().get(0);
+
+        JUnitSoftAssertions softly = new JUnitSoftAssertions();
+        softly.assertThat(loan.getCopyId())
+                .as("Copy ID of the loan persisted to database")
+                .isEqualTo(copy.getId());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @Transactional
+    public void testCreateLoanWithNoCopyID() {
+        Copy copy = new Copy(Long.valueOf(1), "Book", 4);
+        resourceFactory.createLoanForCopy(copy, "DylanRodgers98");
     }
 }
