4fd25afeba3b17d5658c2b8ee671e83dab540a53
Dylan Rodgers <dylanirodgers@aol.com>
2020-02-26 21:52:04 +0000

    Add global exception handler

--- src/main/java/com/crowvalley/tawelib/Main.java
+++ src/main/java/com/crowvalley/tawelib/Main.java
@@ -5,8 +5,12 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     public static final String LOGIN_PAGE_FXML = "/fxml/login.fxml";
 
@@ -22,7 +26,18 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler(new GlobalExceptionHandler());
         launch(args);
+    }
+
+    private static class GlobalExceptionHandler implements Thread.UncaughtExceptionHandler {
+
+        @Override
+        public void uncaughtException(Thread thread, Throwable throwable) {
+            LOGGER.error("An uncaught exception occurred on thread: {}", thread, throwable);
+            FXMLUtils.displayErrorDialogBox("System Error", "A fatal system error has occurred!" + System.lineSeparator() + throwable);
+        }
+
     }
 
 }
