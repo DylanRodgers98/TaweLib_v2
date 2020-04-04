791208db6b9f67c618460808e93f846d52f27203
Dylan Rodgers <dylanirodgers@aol.com>
2020-04-04 14:46:36 +0100

    Set onActions in FXML for LogOut tab

--- src/main/java/com/crowvalley/tawelib/controller/librarian/LibrarianLogOutController.java
+++ src/main/java/com/crowvalley/tawelib/controller/librarian/LibrarianLogOutController.java
@@ -2,21 +2,13 @@ package com.crowvalley.tawelib.controller.librarian;
 
 import com.crowvalley.tawelib.Main;
 import com.crowvalley.tawelib.UserContextHolder;
-import com.crowvalley.tawelib.controller.InitializableFXController;
+import com.crowvalley.tawelib.controller.FXController;
 import com.crowvalley.tawelib.util.FXMLUtils;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 
-public class LibrarianLogOutController implements InitializableFXController {
+public class LibrarianLogOutController implements FXController {
 
     @FXML
-    private Button btnLogOut;
-
-    @Override
-    public void initialize() {
-        btnLogOut.setOnAction(e -> logOut());
-    }
-
     private void logOut() {
         UserContextHolder.clear();
         FXMLUtils.loadNewScene(Main.LOGIN_PAGE_FXML);
